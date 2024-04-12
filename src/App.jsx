f4de68769a3d1dd7f7653dab12d80eaaf0e5b88a
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2024-04-12 21:15:30 +0800

    Initital Setup with tailwind and redux

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Counter } from "./features/counter/counter";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Counter></Counter>
+    </>
+  );
+}
+
+export default App;
