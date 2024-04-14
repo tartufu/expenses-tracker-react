c4fab4251e706554b6c70e8b53e9f58a5a53094b
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2024-04-14 16:04:44 +0800

    Added Wouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import "./App.css";
 
 import { Counter } from "./features/counter/counter";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Counter></Counter>
-    </>
-  );
-}
+import { Link, Route } from "wouter";
+
+const App = () => (
+  <div>
+    <Link href="/users/John">
+      <a className="link">My Account</a>
+    </Link>
+
+    <Route path="/inbox" component={Counter} />
+    <Route path="/settings">Settings</Route>
+    <Route path="/users/:name">
+      {(params) => <div>Hello, {params.name}!</div>}
+    </Route>
+  </div>
+);
 
 export default App;
